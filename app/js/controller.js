5a6a6e67f31bb95d71b1c082e09a1716b0759957
Manu Mouton <manumouton@gmail.com>
2015-03-10 22:34:31 +0100

    gestion de la navigation, simplification google maps

--- app/js/controller.js
+++ app/js/controller.js
@@ -15,48 +15,69 @@ weddingApp.controller("navigation" ,function ($scope) {
     ];
 });
 
+weddingApp.controller("navigationButton" ,function ($scope, $location) {
+    $scope.getPreviousPage = function(){
+        var path = $location.path();
+        if(path==='/home'){
+            return '#/rsvp';
+        }
+        else if(path==='/day/ceremony'){
+            return '#/home';
+        }
+        else if(path==='/day/reception'){
+            return '#/day/ceremony';
+        }
+        else if(path==='/list'){
+            return '#/day/reception';
+        }
+        else if(path==='/rsvp'){
+            return '#/list';
+        }
+    };
+    $scope.getNextPage = function(){
+        var path = $location.path();
+        if(path==='/home'){
+            return '#/day/ceremony';
+        }
+        else if(path==='/day/ceremony'){
+            return '#/day/reception';
+        }
+        else if(path==='/day/reception'){
+            return '#/list';
+        }
+        else if(path==='/list'){
+            return '#/rsvp';
+        }
+        else if(path==='/rsvp'){
+            return '#/home';
+        }
+    };
+});
+
+weddingApp.controller("mainController" ,function ($scope) {
+
+});
+
 weddingApp.controller("ceremonyController" ,function ($scope) {
-    $scope.pageClass = 'page-home';
-    $scope.map = { center: { latitude: 50.6292333, longitude: 5.1148211 }, zoom: 16 };
-    $scope.options = {scrollwheel: false};
-    $scope.marker = {
-        id: 0,
-        coords: {
-            latitude: 50.6292333,
-            longitude: 5.1148211
-        },
-        options: { draggable: false, title: "Eglise d'Avennes" }
-    };
-
-
-    $scope.windowOptions = {
-        visible: true
-    };
-
-    $scope.onClick = function() {
-        $scope.windowOptions.visible = !$scope.windowOptions.visible;
-    };
-
-    $scope.closeClick = function() {
-        $scope.windowOptions.visible = false;
-    };
-
-    $scope.title = "Eglise d'Avennes";
-
-    $scope.url = 'https://www.google.com/maps/place/Rue+de+la+Justice+de+Paix+3,+4260+Braives,+Belgique/@50.6292333,5.1148211,17z/data=!3m1!4b1!4m2!3m1!1s0x47c10918870b926b:0xa240039843ce73a2';
+    buildScopeForMap($scope, 50.6292333, 5.1148211, 16, false, "Eglise d'Avennes",
+        'https://www.google.com/maps/place/Rue+de+la+Justice+de+Paix+3,+4260+Braives,+Belgique/@50.6292333,5.1148211,17z/data=!3m1!4b1!4m2!3m1!1s0x47c10918870b926b:0xa240039843ce73a2');
 });
 
 weddingApp.controller("receptionController" ,function ($scope) {
-    $scope.pageClass = 'page-home';
-    $scope.map = { center: { latitude: 50.618876, longitude: 5.111879 }, zoom: 16 };
-    $scope.options = {scrollwheel: false};
+    buildScopeForMap($scope, 50.618876, 5.111879, 16, false, 'Ferme des sources',
+        'https://www.google.com/maps/place/Rue+des+Aiwisses+18,+4260+Braives,+Belgique/@50.618876,5.111879,14z/data=!4m2!3m1!1s0x47c109056dca6cc7:0x8df128cc9cc79055');
+});
+
+function buildScopeForMap($scope, latitude, longitude, zoom, scrollwheel, title, url){
+    $scope.map = { center: { latitude: latitude, longitude: longitude}, zoom: zoom };
+    $scope.options = {scrollwheel: scrollwheel};
     $scope.marker = {
         id: 0,
         coords: {
-            latitude: 50.618876,
-            longitude: 5.111879
+            latitude: latitude,
+            longitude: longitude
         },
-        options: { draggable: false, title: 'Ferme des Sources' }
+        options: { draggable: false, title: title }
     };
 
     $scope.windowOptions = {
@@ -71,21 +92,15 @@ weddingApp.controller("receptionController" ,function ($scope) {
         $scope.windowOptions.visible = false;
     };
 
-    $scope.title = 'Ferme des sources';
-
-    $scope.url = 'https://www.google.com/maps/place/Rue+des+Aiwisses+18,+4260+Braives,+Belgique/@50.618876,5.111879,14z/data=!4m2!3m1!1s0x47c109056dca6cc7:0x8df128cc9cc79055';
-});
+    $scope.title = title;
+    $scope.url = url;
+}
 
 
 weddingApp.controller("listController" ,function ($scope) {
     $scope.url = "http://www.kadolog.be/caroetmanusemarient";
-    $scope.pageClass = "page-home";
-});
-
-weddingApp.controller("mainController" ,function ($scope) {
-    $scope.pageClass = "page-home";
 });
 
 weddingApp.controller("rsvpController" ,function ($scope) {
-    $scope.pageClass = "page-home";
+
 });
